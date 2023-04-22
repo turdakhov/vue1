ee5e39576299c44ec398b62828d3e0a3b30c24eb
turdakhov <turdakhov@gmail.com>
2023-04-22 19:53:59 +0600

    initial commit

---
+++ resources/js/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <h2>Assignments</h2>
+    <ul>
+        <li>Finish project</li>
+        <li>Read chapter 4</li>
+        <li>Turn in homework</li>
+    </ul>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            active: false,
+        };
+    },
+
+    methods: {
+        toggle() {
+            this.active = ! this.active;
+        }
+    }
+}
+</script>
+<style>
+    .text-red {
+        color: red;
+    }
+    .text-green {
+        color: green;
+    }
+</style>
